a82f1b0409402400670161a3ddc1a55112552d14
smimdla0205 <sangmin0205@gmail.com>
2025-01-25 20:09:01 +0900

    feat: button 컴포넌트 예시 완성 #7

---
+++ components/common/button.tsx
@@ -0,0 +1,44 @@
+"use client";
+import type { ButtonHTMLAttributes } from "react";
+
+import styles from "./button.module.css";
+
+import Image from "next/image";
+import classNames from "classnames/bind";
+
+interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "type" | "onClick"> {
+  type: "button";
+  text: string;
+  onClick: () => void;
+  className?: string;
+  color: "green" | "blackGreen" | "white" | "gray";
+  width?: string;
+  height?: string;
+  icon?: boolean;
+  iconSrc?: string;
+  iconFill?: string;
+}
+
+const cx = classNames.bind(styles);
+
+export default function Button({
+  text,
+  onClick,
+  className,
+  width = "100%",
+  height = "40px",
+  color = "green",
+  icon = false,
+  iconSrc = "default.svg",
+}: ButtonProps) {
+  const classNames = cx("button", className, {
+    [`button__${color}`]: color,
+  });
+  const iconPath = `/icon/${iconSrc}`;
+  return (
+    <button onClick={onClick} className={classNames} style={{ width, height }}>
+      {icon && <Image src={iconPath} alt="buttonIcon" width={20} height={20} />}
+      {text}
+    </button>
+  );
+}
