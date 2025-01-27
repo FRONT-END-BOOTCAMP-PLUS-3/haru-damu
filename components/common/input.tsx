b6898c71cb84f22d04a898634e9edb86263a14c8
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-01-28 01:02:42 +0900

    feat : label이 포함된 input 컴포넌트 구현 #11

---
+++ components/common/input.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useState } from "react";
+import type { ChangeEvent, InputHTMLAttributes } from "react";
+
+import style from "@/components/common/input.module.css";
+
+import classNames from "classnames";
+
+const cx = classNames.bind(style);
+
+interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "onChange"> {
+  label: string;
+  direction?: "vertical" | "horizontal";
+  placeholder?: string;
+  inputValue?: string | number;
+  onChange?: (value: string | number) => void;
+  waringMsg?: string;
+}
+
+export default function Input({
+  label,
+  direction = "horizontal",
+  type = "text",
+  placeholder = "값을 입력해주세요.",
+  inputValue = "",
+  onChange,
+  waringMsg,
+  ...rest
+}: InputProps) {
+  const { wrapper, vertical, horizontal, input_label, msg_wrapper, input, warning_msg } = style;
+
+  const [value, setValue] = useState<string | number>(inputValue);
+
+  const isVertical = direction === "vertical";
+
+  // onChange가 들어오지 않아도 기본 동작을 합니다.
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>): void => {
+    const value = event.target.value;
+
+    setValue(value);
+    onChange && onChange(value);
+  };
+
+  return (
+    <div className={cx(wrapper, isVertical ? vertical : horizontal)}>
+      <label htmlFor={label} className={cx(input_label, "text-md-b")}>
+        {label}
+      </label>
+      <div className={cx(msg_wrapper, vertical)}>
+        <input
+          id={label}
+          type={type}
+          className={cx(input, "text-md")}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChangeHandler}
+          {...rest}
+        />
+        {waringMsg && <span className={`${warning_msg} text-sm`}>{waringMsg}</span>}
+      </div>
+    </div>
+  );
+}
