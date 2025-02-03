f2dc43978b71a3ddaeaf721826f5bc6b70d73889
sakedon2151 <sakedon2151@gmail.com>
2025-02-03 14:38:00 +0900

    feat : styles 구조분해할당으로 변경

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -6,24 +6,25 @@ import Link from "next/link";
 import Image from "next/image";
 import classNames from "classnames/bind";
 import { ExternalLink } from "lucide-react";
-import logo from "@/public/haruDamu_logo.svg";
+import logo from "@/public/haruDamu_grayscale_logo.svg";
 
 const cx = classNames.bind(styles);
 
 export default function Footer() {
+  const { wrapper, footer, footer__left, footer__center, footer__right } = styles;
   return (
-    <footer className={styles.wrapper}>
-      <div className={cx(`footer`, `container`)}>
-        <Link href={"/"} className={styles.footer__left}>
-          <Image src={logo} alt="logo" width={50} height={50} className={cx(`footer__left-img`)} />
+    <footer className={wrapper}>
+      <div className={cx(footer, `container`)}>
+        <Link href={"/"} className={footer__left}>
+          <Image src={logo} alt="logo" width={50} height={50} />
           <p className="title-lg-b">하루담은</p>
         </Link>
-        <div className={styles.footer__center}>
+        <div className={footer__center}>
           <p className="text-md">Copyright © {new Date().getFullYear()} - All right reserved</p>
         </div>
         <Link
           href={"https://github.com/FRONT-END-BOOTCAMP-PLUS-3/haru-damu"}
-          className={styles.footer__right}
+          className={footer__right}
           target="_blank"
           prefetch={false}
           rel="noopener noreferrer"
