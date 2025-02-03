1f9b302e390247f31a8ca2b7ae2e14715a2413b3
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-03 17:24:51 +0900

    feat : 버티컬 아이템 뷰 구현 #4

---
+++ components/common/vertical_item.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import type { TItem } from "@/types";
+
+import style from "@/components/common/vertical_item.module.css";
+
+import Link from "next/link";
+import Image from "next/image";
+import classNames from "classnames/bind";
+import Button from "@/components/common/button";
+import { ImageOff, ShoppingBasket } from "lucide-react";
+
+const cx = classNames.bind(style);
+
+interface VerticalItemProps {
+  item: TItem;
+}
+
+export default function VerticalItem({ item }: VerticalItemProps) {
+  const { vertical_item__image__wrapper, vertical_item__no__image, vertical_item__image, vertical_item__link } = style;
+
+  const { item_id, item_name, item_price, img, blurImg } = item;
+
+  return (
+    <li>
+      <Link href={`/item/${item_id}`} className={vertical_item__link}>
+        <div className={cx(vertical_item__image__wrapper, !img && vertical_item__no__image)}>
+          {img ? (
+            <Image
+              src={img}
+              alt={item_name}
+              fill
+              className={vertical_item__image}
+              placeholder="blur"
+              blurDataURL={blurImg}
+            />
+          ) : (
+            <ImageOff size={80} />
+          )}
+        </div>
+        <Button text="담기" iconComponent={<ShoppingBasket />} />
+        <span className="text-lg">{item_name}</span>
+        <span className="text-lg-b">{item_price}원</span>
+      </Link>
+    </li>
+  );
+}
