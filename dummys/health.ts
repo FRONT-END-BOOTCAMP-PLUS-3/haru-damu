ce424acdbd9d4c0dc6d58da1f035241f3001f93c
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-04 14:59:33 +0900

    docs : health 누락 데이터 추가 #40

--- dummys/health.ts
+++ dummys/health.ts
@@ -1,10 +1,18 @@
-const health = {
+import type { THealth } from "@/types";
+
+const health: THealth = {
   user_id: 1,
   gender_code: "M",
   age: 35,
   weight: 75,
   height: 175,
   activity_code: 3,
+  calorie: 2672,
+  carbohydrates: 334,
+  protein: 167,
+  fat: 74,
+  sodium: 2.3,
+  sugar: 50,
   is_custom: false,
   created_at: "2025-01-24T12:00:00",
   updated_at: "2025-01-24T12:00:00",
