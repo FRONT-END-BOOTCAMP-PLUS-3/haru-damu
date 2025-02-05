299d6e85acdbd000b0aaadc7897ff93971603d68
smimdla0205 <sangmin0205@gmail.com>
2025-02-05 15:25:24 +0900

    feat: 불필요한 useEffect 및 useState 임포트 제거2 #3

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useRouter } from "next/navigation"; // ✅ Link 대신 useRouter 사용
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import CategoryList from "@/components/category_list";
 import styles from "@/components/common/header.module.css";
