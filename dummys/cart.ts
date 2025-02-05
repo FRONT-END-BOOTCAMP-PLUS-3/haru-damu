0c7c15b8eb8cda6b5927b376d4eb3f1588b7217d
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-05 16:03:37 +0900

    dummy : 더미 구조를 데이터베이스와 같도록 수정 #30

---
+++ dummys/cart.ts
@@ -0,0 +1,46 @@
+const cartItems = [
+  {
+    item_id: 1,
+    user_id: 1,
+    quantity: 4,
+    isChecked: true,
+    wrapper_id: null,
+  },
+  {
+    item_id: 2,
+    user_id: 1,
+    quantity: 1,
+    isChecked: true,
+    wrapper_id: null,
+  },
+  {
+    item_id: 3,
+    user_id: 1,
+    quantity: 1,
+    isChecked: false,
+    wrapper_id: null,
+  },
+  {
+    item_id: 4,
+    user_id: 1,
+    quantity: 1,
+    isChecked: false,
+    wrapper_id: null,
+  },
+  {
+    item_id: 5,
+    user_id: 1,
+    quantity: 1,
+    isChecked: true,
+    wrapper_id: null,
+  },
+  {
+    item_id: 6,
+    user_id: 1,
+    quantity: 1,
+    isChecked: false,
+    wrapper_id: null,
+  },
+];
+
+export default cartItems;
