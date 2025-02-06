10a129eb6df7d8ccd1aea668b943414ba14b5181
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-06 14:30:20 +0900

    feat : nutrition을 전역 상태로 저장 #26

--- stores/mypage_store.ts
+++ stores/mypage_store.ts
@@ -3,14 +3,32 @@ import type { State } from "@/hooks/usestore";
 
 export type TMypagePath = "nutrition" | "personal" | "health" | "order";
 
-export type TMypageSlice = {
-  mypagePath: TMypagePath;
+export type TMypageNutrition = {
+  key: string;
+  label: string;
+  unit: string;
+  value: number;
+  recommendValue: number;
+};
 
+export type TMypageSlice = {
+  //aside
+  mypagePath: TMypagePath;
   setMypagePath: (mypagePath: TMypagePath) => void;
+
+  //nutrition
+  userNutrition: TMypageNutrition[];
+  setUserNutrition: (userNutrition: TMypageNutrition[]) => void;
 };
 
 export const createMypageSlice: StateCreator<Partial<State>, [], [], TMypageSlice> = (set, get) => ({
+  //aside
   mypagePath: "nutrition",
 
   setMypagePath: (mypagePath) => set({ mypagePath }),
+
+  //nutrition
+  userNutrition: [],
+
+  setUserNutrition: (userNutrition) => set({ userNutrition }),
 });
