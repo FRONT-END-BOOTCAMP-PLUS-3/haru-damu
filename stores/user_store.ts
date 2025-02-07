bd6c886aa65ce1f4ffe15319d28815cd580c42e7
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-07 13:21:16 +0900

    feat : 로그인, 로그아웃 핸들러 강화 #72

--- stores/user_store.ts
+++ stores/user_store.ts
@@ -1,32 +1,73 @@
 import type { StateCreator } from "zustand";
 import type { State } from "@/hooks/usestore";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "";
+
+export type TUser = {
+  id: number;
+  name: string;
+  email: string;
+  phone?: string;
+  address?: string;
+} | null;
+
+export type TUserType = "user" | "partner" | null;
+
 export type TUserSlice = {
+  user: TUser;
+  userImg: string | null;
+
   isLogin: boolean;
-  userType: "user" | "partner" | null;
+  userType: TUserType;
   isShrunk: boolean;
 
-  login: (type: "user" | "partner") => void;
+  login: (type: TUserType, user?: Omit<TUser, "id">, img?: string) => void;
   logout: () => void;
   setIsShrunk: (isShrunk: boolean) => void;
 };
 
 export const createUserSlice: StateCreator<Partial<State>, [], [], TUserSlice> = (set) => ({
+  user: null,
+  userImg: null,
+
   isLogin: false,
   userType: null,
   isShrunk: false,
 
-  login: (type) =>
-    set((state) => ({
-      ...state,
-      isLoggedIn: true,
-      userType: type,
-    })),
+  login: async (type, user, img) => {
+    console.log(type);
+    // 유저 로그인 타입이 넘어오지 않으면 로그인 되지 않았다고 판단합니다.
+    const isLogin = !!type && type !== null;
+    try {
+      const response =
+        isLogin &&
+        (await fetch(`${BASE_URL}/api/users`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            type,
+            ...user,
+          }),
+        }));
+
+      const data = response ? await response.json() : null;
+
+      set({
+        user: data.user,
+        userImg: img,
+        isLogin,
+        userType: type,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
 
   logout: () =>
-    set((state) => ({
-      ...state,
-      isLoggedIn: false,
+    set(() => ({
+      user: null,
+      userImg: null,
+      isLogin: false,
       userType: null,
     })),
 
