dd584b5efe4e7a5710d8e02ee21405e5145562c2
Haeran Kim <116875965+kimmand0o0@users.noreply.github.com>
2025-02-07 13:17:18 +0900

    feat : google에서 제송하는 user_img를 사용할 수 있도록 설정 #72

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cmkyznelxslzvzqlncud.supabase.co",
-        port: "",
-        pathname: "/storage/**",
-        search: "",
-      },
-    ],
+    domains: ["cmkyznelxslzvzqlncud.supabase.co", "lh3.googleusercontent.com"],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "cmkyznelxslzvzqlncud.supabase.co",
+    //     port: "",
+    //     pathname: "/storage/**",
+    //     search: "",
+    //   },
+    //   {
+    //     protocol: "https",
+    //     hostname: "lh3.googleusercontent.com",
+    //     pathname: "/a/**",
+    //   },
+    // ],
   },
 };
 
