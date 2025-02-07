bc9eaf6c6704a6b9d7e26efdf55f3bbd7f5d618e
sakedon2151 <sakedon2151@gmail.com>
2025-02-08 00:26:36 +0900

    feat : 주문상세 페이지 뷰 구현 #32

---
+++ app/(main)/order/[id]/_components/payment_info_section.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+import Button from "@/components/common/button";
+import Subheading from "@/components/common/subheading";
+import LabelValueText from "@/components/common/label_value_text";
+
+import styles from "@/app/(main)/order/[id]/_components/payment_info_section.module.css";
+
+import classNames from "classnames/bind";
+import { ChevronLeft } from "lucide-react";
+
+interface PaymentInfoSectionProps {
+  totalPrice: string;
+}
+
+const cx = classNames.bind(styles);
+
+export default function PaymentInfoSection({ totalPrice }: PaymentInfoSectionProps) {
+  const router = useRouter();
+  const goForwardHandler = () => {
+    router.back();
+  };
+  return (
+    <section>
+      <Subheading title={"결제 정보"} />
+      <LabelValueText label="결제 금액" value={`${totalPrice} 원`} />
+      {/* 추후 payments 테이블에 대한 개발 진행 시 결제 내역에 결제 수단 출력 고려 */}
+      <Button
+        iconComponent={<ChevronLeft size={16} />}
+        color="primary"
+        width="252px"
+        height="60px"
+        text={"주문 목록으로 돌아가기"}
+        className={cx("payment_info_section__forward_button")}
+        onClick={goForwardHandler}
+      />
+    </section>
+  );
+}
---
+++ app/(main)/order/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { useParams } from "next/navigation";
+
+import OrderItemSection from "@/components/order_item_section";
+import ShippingInfoSection from "@/components/shipping_info_section";
+import PaymentInfoSection from "@/app/(main)/order/[id]/_components/payment_info_section";
+
+import totalPriceCalculator from "@/utils/total_price_calculator";
+
+import styles from "@/app/(main)/order/[id]/page.module.css";
+
+import users from "@/dummys/users";
+import classNames from "classnames/bind";
+import horizontalItems from "@/dummys/horizontal_items";
+
+const cx = classNames.bind(styles);
+
+export default function OrderPage({ params }: { params: { id: number } }) {
+  const totalPrice = totalPriceCalculator(horizontalItems);
+  return (
+    <div className={cx("container")}>
+      <h2 className={cx("order_title__h2", "title-lg-b")}>주문상세</h2>
+      <OrderItemSection items={horizontalItems} totalPrice={totalPrice} />
+      <ShippingInfoSection user={users[1]} />
+      <PaymentInfoSection totalPrice={totalPrice} />
+    </div>
+  );
+}
