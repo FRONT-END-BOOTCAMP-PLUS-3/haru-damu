cb0567eed470eabeac0b33f0da1494962e02bf86
BoyunLee <https://github.com/BoyunLee>
2025-02-10 21:15:29 +0900

    fix : 회원탈퇴기능 추가

--- app/(main)/mypage/_components/personal.tsx
+++ app/(main)/mypage/_components/personal.tsx
@@ -4,7 +4,13 @@ import { useStore } from "@/hooks/usestore";
 
 import styles from "./personal.module.css";
 
-import { PERSONAL_BUTTON, PERSONAL_EMAIL, PERSONAL_FIELDS, PERSONAL_TITLE } from "@/constants/mypage";
+import {
+  PERSONAL_BUTTON,
+  PERSONAL_DELETE_BUTTON,
+  PERSONAL_EMAIL,
+  PERSONAL_FIELDS,
+  PERSONAL_TITLE,
+} from "@/constants/mypage";
 
 import classNames from "classnames/bind";
 
@@ -17,7 +23,7 @@ export default function PersonalPage() {
     Object.fromEntries(PERSONAL_FIELDS.map(({ key }) => [key, ""])), // 초기값은 빈 값으로 설정
   );
 
-  const userId = 1; // 예시: 사용자 ID를 1로 설정
+  const userId = 2; // 예시: 사용자 ID를 1로 설정
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -60,6 +66,23 @@ export default function PersonalPage() {
     }
   };
 
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`/api/mypage/personal/${userId}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to delete user data");
+      }
+      addMessage("탈퇴되었습니다.");
+      window.location.href = "/";
+      
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className={cx("personal")}>
       <h2 className={cx("personal__title", "title-lg-b")}>{PERSONAL_TITLE}</h2>
@@ -86,6 +109,9 @@ export default function PersonalPage() {
 
       <button className={cx("personal__button", "text-md")} onClick={handleSave}>
         {PERSONAL_BUTTON}
+      </button>
+      <button className={cx("personal__delete_button", "text-md")} onClick={handleDelete}>
+        {PERSONAL_DELETE_BUTTON}
       </button>
     </div>
   );
