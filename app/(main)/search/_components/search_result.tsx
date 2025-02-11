d1f7cfebe927e259f82d6600e4ed076002cb351c
sakedon2151 <sakedon2151@gmail.com>
2025-02-12 02:52:57 +0900

    fix : search page 및 하위 컴포넌트 패칭 구성, 로직 변경 #106

--- app/(main)/search/_components/search_result.tsx
+++ app/(main)/search/_components/search_result.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import ItemList from "@/components/common/item_list";
 
 import { SEARCH_TITLE } from "@/constants/categories";
@@ -5,10 +9,30 @@ import { SEARCH_TITLE } from "@/constants/categories";
 import type { TItem } from "@/types";
 
 interface SearchResultProps {
-  items: TItem[];
-  totalItems: number;
+  query: string;
+  page: number;
 }
 
-export default function SearchResult({ items, totalItems }: SearchResultProps) {
-  return <ItemList items={items} totalItems={totalItems} title={SEARCH_TITLE} baseUrl="/search" />;
+export default function SearchResult({ query, page }: SearchResultProps) {
+  const [items, setItems] = useState<TItem[]>([]);
+
+  useEffect(() => {
+    if (!query) return;
+    const fetchItems = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/search?query=${encodeURIComponent(query)}&page=${page}`,
+          {
+            method: "GET",
+          },
+        ).then((response) => response.json());
+        setItems(res);
+      } catch (error) {
+        console.error("검색어 아이템 리스트 fetch 실패", error);
+      }
+    };
+    fetchItems();
+  }, [query, page]);
+
+  return <ItemList items={items} currentPage={page} title={SEARCH_TITLE} baseUrl="/search" />;
 }
