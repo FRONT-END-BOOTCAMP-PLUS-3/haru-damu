3b5d7adc3bca3a29e863b88ab88ab9a2a1d07c6b
sakedon2151 <sakedon2151@gmail.com>
2025-02-12 04:35:56 +0900

    fix : page 영역 단순화 및 searchParams 지정 #106

--- app/(main)/category/page.tsx
+++ app/(main)/category/page.tsx
@@ -1,31 +1,9 @@
 import CategoryResult from "@/app/(main)/category/_components/category_result";
 
-import getBlurImg from "@/utils/get_blur_img";
+export default async function CategoryPage({ searchParams }: { searchParams: { value: string; page?: string } }) {
+  const params = await searchParams;
+  const currentCategory = params.value;
+  const currentPage = Number(params.page) || 1;
 
-import type { TItem } from "@/types";
-
-import items from "@/dummys/items";
-
-export default async function CategoryPage({ searchParams }: { searchParams: { page?: string } }) {
-  const itemsPerPage = 20;
-  const currentPage = Number(searchParams.page) || 1;
-
-  // 페이지네이션 유효성 검사
-  const totalPages = Math.ceil(items.length / itemsPerPage);
-  if (currentPage < 1 || currentPage > totalPages) {
-    return { notFound: true };
-  }
-
-  // 현재 페이지에 해당하는 아이템들만 가져오기
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const paginatedItems = items.slice(startIndex, endIndex);
-
-  const newItems: TItem[] = [];
-  for (const item of paginatedItems) {
-    const blurImg = await getBlurImg(item.img);
-    newItems.push({ ...item, blurImg, img: item.img });
-  }
-
-  return <CategoryResult items={newItems} totalItems={items.length} />;
+  return <CategoryResult category={currentCategory} page={currentPage} />;
 }
--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,8 +1,9 @@
 import SearchResult from "@/app/(main)/search/_components/search_result";
 
 export default async function SearchPage({ searchParams }: { searchParams: { query: string; page?: string } }) {
-  const currentQuery = searchParams.query; // 이건 현재 검색어를 할당
-  const currentPage = Number(searchParams.page) || 1; // 이건 현재 페이지네이션 위치를 할당 || 없으면 1로 설정?
+  const params = await searchParams;
+  const currentQuery = params.query;
+  const currentPage = Number(params.page) || 1;
 
   return <SearchResult query={currentQuery} page={currentPage} />;
 }
